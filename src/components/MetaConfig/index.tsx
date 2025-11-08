c8d22fd2dd21af7a503249489236cfe8f2a200af
Mazeriio <110097914+Mazetta@users.noreply.github.com>
2025-11-08 19:06:19 +0000

    probably stupid, but if it works, it works

--- src/components/MetaConfig/index.tsx
+++ src/components/MetaConfig/index.tsx
@@ -34,6 +34,23 @@ const MetaConfig: React.FC<MetaConfigProps> = (props) => {
         <>
           <meta property="article:published_time" content={props.date} />
           <meta property="article:author" content={CONFIG.profile.name} />
+          <title>{props.title}</title>
+          <meta name="robots" content="follow, index" />
+          <meta charSet="UTF-8" />
+          <meta name="description" content={props.description} />
+          {/* og */}
+          <meta property="og:type" content={props.type} />
+          <meta property="og:title" content={props.title} />
+          <meta property="og:description" content={props.description} />
+          <meta property="og:url" content={props.url} />
+          {CONFIG.lang && <meta property="og:locale" content={CONFIG.lang} />}
+          {props.image && <meta property="og:image" content={props.image} />}
+          {/* twitter */}
+          <meta name="twitter:title" content={props.title} />
+          <meta name="twitter:description" content={props.description} />
+          <meta name="twitter:card" content="summary_large_image" />
+          {props.image && <meta name="twitter:image" content={props.image} />}
+          {/* post */}
         </>
       )}
       <link
