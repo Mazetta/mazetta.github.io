36a5f8280920744753319b2886fcbf2e644852e3
Mazeriio <110097914+Mazetta@users.noreply.github.com>
2025-11-09 19:30:55 +0000

    test: revert changes

--- src/layouts/RootLayout/Header/Logo.tsx
+++ src/layouts/RootLayout/Header/Logo.tsx
@@ -1,48 +1,15 @@
 import Link from "next/link"
-import Image from "next/image"
 import { CONFIG } from "site.config"
 import styled from "@emotion/styled"
 
 const Logo = () => {
   return (
     <StyledWrapper href="/" aria-label={CONFIG.blog.title}>
-      <IconWrapper>
-        <Image
-          src={CONFIG.blog.icon}
-          alt=""
-          width={18}
-          height={18}
-          style={{ objectFit: "contain" }}
-        />
-      </IconWrapper>
-      <Title>{CONFIG.blog.title}</Title>
+      {CONFIG.blog.emoji} {CONFIG.blog.title}
     </StyledWrapper>
   )
 }
 
 export default Logo
 
-const StyledWrapper = styled(Link)`
-  display: flex;
-  align-items: center;
-  gap: 0.5rem;
-  text-decoration: none;
-  font-weight: 600;
-  font-size: 1rem; 
-  line-height: 1.25rem; 
-  color: ${({ theme }) => theme.colors.gray12};
-`
-
-const IconWrapper = styled.span`
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  height: 1.25rem; 
-  transform: translateY(1px); 
-`
-
-const Title = styled.span`
-  display: flex;
-  align-items: center;
-  line-height: 1.25rem;
-`
+const StyledWrapper = styled(Link)``
