1c3fc7cb41a789643a3110b40a0eedc7c919b13e
Mazeriio <110097914+Mazetta@users.noreply.github.com>
2025-11-11 14:06:11 +0000

    test: scroll fix?

--- src/routes/Feed/ContactCard.tsx
+++ src/routes/Feed/ContactCard.tsx
@@ -97,6 +97,7 @@ const StyledTitle = styled.div`
   margin-bottom: 0.75rem;
 `
 const StyledWrapper = styled.div`
+  overflow: auto;
   display: flex;
   padding: 0.25rem;
   flex-direction: column;
